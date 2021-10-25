58f75f6b2cce4bb18292a87743166fd109111480
ufuk-ozdek <70910355+ufuk-ozdek@users.noreply.github.com>
2021-10-25 11:53:11 +0300

    Update Radix(base_10).py

--- Radix(base_10).py
+++ Radix(base_10).py
@@ -1,37 +1,23 @@
-
-
 arr = [170, 45, 75, 90, 802, 24, 2, 66]
 
 
 def radix_sort(arr):
-    base_10 = []
-    for i in range(10):
-        base_10.append([])
-    temp = []
-    for i in range(10):
-        temp.append([])
     x = True
     y = max(arr)
     d = 1
     while x:
-
         if y / 10 > 1:
             d += 1
             y = y / 10
-
         else:
-            x = False
             break
-
     for j in range(d):
         output = []
-        base_10 = []
-        for i in range(10):
-            base_10.append([])
+        base_10 = [[]for i in range(10)]
         for i in arr:
-            x = i % 10**(j+1)
-            x = x // 10**j
-            base_10[x].append(i)
+            k = i % 10**(j+1)
+            k = k // 10**j
+            base_10[k].append(i)
         for l in base_10:
             output.extend(l)
 
@@ -41,5 +27,4 @@ def radix_sort(arr):
 
 
 print(radix_sort(arr))
-
 
